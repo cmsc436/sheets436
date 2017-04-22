a9e66e9075eb2bb1bce9d27d5072c89a2f837add
Jeremy Krach <jakrach@umd.edu>
2017-04-22 17:36:11 -0400

    Improved sample app to allow for more robust testing.

--- app/src/main/java/edu/umd/sheets436/MainActivity.java
+++ app/src/main/java/edu/umd/sheets436/MainActivity.java
@@ -8,6 +8,9 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -22,19 +25,29 @@ public class MainActivity extends AppCompatActivity implements Sheets.Host {
     public static final int LIB_PERMISSION_REQUEST_CODE = 1003;
     public static final int LIB_PLAY_SERVICES_REQUEST_CODE = 1004;
     public static final int LIB_CONNECTION_REQUEST_CODE = 1005;
+    private static final int MIN = 0;
+    private static final int MAX = 100;
 
     private Sheets sheet;
+    private Spinner spinner;
+
+    private double random() {
+        double range = Math.abs(MAX - MIN);
+        return (Math.random() * range) + MIN;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        sheet = new Sheets(this, this, getString(R.string.app_name), getString(R.string.CMSC436_testing_spreadsheet), getString(R.string.CMSC436_private_test_spreadsheet));
-        sheet.writeData(Sheets.TestType.LH_TAP, getString(R.string.user_id), 1.23f);
+        spinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.test_array, android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
 
-        float[] trialData = {1.23f, 4.56f, 7.89f};
-        sheet.writeTrials(Sheets.TestType.LH_TAP, getString(R.string.user_id), trialData);
+        sheet = new Sheets(this, this, getString(R.string.app_name), getString(R.string.CMSC436_testing_spreadsheet), getString(R.string.CMSC436_private_test_spreadsheet));
 
         // The next two lines of code allows network calls on the UI thread. Do not do this in a
         // real app. I'm just doing this for ease of coding/reading, since this is a sample of how
@@ -52,6 +65,59 @@ public class MainActivity extends AppCompatActivity implements Sheets.Host {
         }
 
         sheet.uploadToDrive(getString(R.string.CMSC436_test_folder), getString(R.string.image_name), bitmap);
+    }
+
+    public void sendToSheets(View v) {
+        float f = (float)random();
+        float[] trialData = {(float)random(), (float)random(), (float)random()};
+        switch (spinner.getSelectedItem().toString()) {
+            case "Tap (Hands)":
+                sheet.writeData(Sheets.TestType.LH_TAP, getString(R.string.user_id), f);
+                sheet.writeData(Sheets.TestType.RH_TAP, getString(R.string.user_id), f);
+                sheet.writeTrials(Sheets.TestType.LH_TAP, getString(R.string.user_id), trialData);
+                sheet.writeTrials(Sheets.TestType.RH_TAP, getString(R.string.user_id), trialData);
+                break;
+            case "Tap (Feet)":
+                sheet.writeData(Sheets.TestType.LF_TAP, getString(R.string.user_id), f);
+                sheet.writeData(Sheets.TestType.RF_TAP, getString(R.string.user_id), f);
+                sheet.writeTrials(Sheets.TestType.LF_TAP, getString(R.string.user_id), trialData);
+                sheet.writeTrials(Sheets.TestType.RF_TAP, getString(R.string.user_id), trialData);
+                break;
+            case "Spiral":
+                sheet.writeData(Sheets.TestType.LH_SPIRAL, getString(R.string.user_id), f);
+                sheet.writeData(Sheets.TestType.RH_SPIRAL, getString(R.string.user_id), f);
+                sheet.writeTrials(Sheets.TestType.LH_SPIRAL, getString(R.string.user_id), trialData);
+                sheet.writeTrials(Sheets.TestType.RH_SPIRAL, getString(R.string.user_id), trialData);
+                break;
+            case "Level":
+                sheet.writeData(Sheets.TestType.LH_LEVEL, getString(R.string.user_id), f);
+                sheet.writeData(Sheets.TestType.RH_LEVEL, getString(R.string.user_id), f);
+                sheet.writeTrials(Sheets.TestType.LH_LEVEL, getString(R.string.user_id), trialData);
+                sheet.writeTrials(Sheets.TestType.RH_LEVEL, getString(R.string.user_id), trialData);
+                break;
+            case "Balloon":
+                sheet.writeData(Sheets.TestType.LH_POP, getString(R.string.user_id), f);
+                sheet.writeData(Sheets.TestType.RH_POP, getString(R.string.user_id), f);
+                sheet.writeTrials(Sheets.TestType.LH_POP, getString(R.string.user_id), trialData);
+                sheet.writeTrials(Sheets.TestType.RH_POP, getString(R.string.user_id), trialData);
+                break;
+            case "Curl":
+                sheet.writeData(Sheets.TestType.LH_CURL, getString(R.string.user_id), f);
+                sheet.writeData(Sheets.TestType.RH_CURL, getString(R.string.user_id), f);
+                sheet.writeTrials(Sheets.TestType.LH_CURL, getString(R.string.user_id), trialData);
+                sheet.writeTrials(Sheets.TestType.RH_CURL, getString(R.string.user_id), trialData);
+                break;
+            case "Sway":
+                sheet.writeData(Sheets.TestType.HEAD_SWAY, getString(R.string.user_id), f);
+                sheet.writeTrials(Sheets.TestType.HEAD_SWAY, getString(R.string.user_id), trialData);
+                break;
+            case "Walk (Indoors)":
+                sheet.writeData(Sheets.TestType.INDOOR_WALKING, getString(R.string.user_id), f);
+                sheet.writeTrials(Sheets.TestType.INDOOR_WALKING, getString(R.string.user_id), trialData);
+                break;
+            default:
+                break;
+        }
     }
 
     @Override
